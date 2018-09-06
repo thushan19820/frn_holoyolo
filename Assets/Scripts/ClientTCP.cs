53c4afcf878b2f2e469d9b6f67ad4329b968bd5d
Thushan <t.satkunanathan@outlook.com>
2018-09-06 16:01:40 +0200

    change to client

--- Assets/Scripts/ClientTCP.cs
+++ Assets/Scripts/ClientTCP.cs
@@ -1,94 +1,43 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using System;
+using System.IO;
 
 #if !UNITY_EDITOR
-    using Windows.Networking;
-    using Windows.Networking.Sockets;
-    using Windows.Storage.Streams;
-#endif
+using Windows.Networking.Sockets;
+using Windows.Networking;
+using Windows.Networking.Connectivity;
 
-//Able to act as a reciever 
+#endif
 public class ClientTCP : MonoBehaviour
 {
-    public String _input = "Waiting";
     public String message_display;
 #if !UNITY_EDITOR
-        StreamSocket socket;
-        StreamSocketListener listener;
-        String port;
-        String message;
-#endif
+private async void Start()
+    {
 
+        Windows.Networking.Sockets.StreamSocket socket = new Windows.Networking.Sockets.StreamSocket();
+        Windows.Networking.HostName server = new Windows.Networking.HostName("172.18.0.137");
+        string serverPort = "8000";
+        await socket.ConnectAsync(server, serverPort);
+
+        Stream streamOut = socket.OutputStream.AsStreamForWrite();
+        StreamWriter writer = new StreamWriter(streamOut);
+        string message = "Hallo Server";
+        message_display = message;
+        await writer.WriteLineAsync(message);
+        await writer.FlushAsync();
+
+
+
+    }
+#else
     // Use this for initialization
     void Start()
     {
-#if !UNITY_EDITOR
-        listener = new StreamSocketListener();
-        port = "9090";
-        listener.ConnectionReceived += Listener_ConnectionReceived;
-        listener.Control.KeepAlive = false;
-
-        Listener_Start();
+        Debug.Log("hallo wolrd");
+        Debug.Log(message_display);
+    }
 #endif
-    }
-
-#if !UNITY_EDITOR
-    private async void Listener_Start()
-    {
-        Debug.Log("Listener started");
-        try
-        {
-            await listener.BindServiceNameAsync(port);
-        }
-        catch (Exception e)
-        {
-            Debug.Log("Error: " + e.Message);
-        }
-
-        Debug.Log("Listening");
-    }
-
-    private async void Listener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
-    {
-        Debug.Log("Connection received");
-
-        try
-        {
-            while (true) {
-
-                    using (var dw = new DataWriter(args.Socket.OutputStream))
-                    {
-                        dw.WriteString("Hello There");
-                        await dw.StoreAsync();
-                        dw.DetachStream();
-                    }  
-
-                    using (var dr = new DataReader(args.Socket.InputStream))
-                    {
-                        dr.InputStreamOptions = InputStreamOptions.Partial;
-                        await dr.LoadAsync(12);
-                        var input = dr.ReadString(12);
-                        Debug.Log("received: " + input);
-                        _input = input;
-
-                    }
-            }
-        }
-        catch (Exception e)
-        {
-            Debug.Log("disconnected!!!!!!!! " + e);
-        }
-
-    }
-
-#endif
-
-    void Update()
-    {
-        this.GetComponent<TextMesh>().text = _input;
-        message_display = _input;
-    }
 }
